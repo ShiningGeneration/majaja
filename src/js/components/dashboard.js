b96c086ab4e9abba0cd89c40e885884602c42c5f
Ricky Chien <ricky060709@gmail.com>
2015-08-17 23:34:36 +0800

    Calculate dashboard items programmatically

--- src/js/components/dashboard.js
+++ src/js/components/dashboard.js
@@ -28,45 +28,103 @@ export default class Dashboard extends React.Component {
   }
 
   render() {
+    let ordereds = [
+      {
+        id: 1,
+        title: '08/06 (星期四) Mon. 丁媽 虱目魚店',
+        products: [
+          { name: '香煎魚肚', amount: 1, price: 100 }
+        ]
+      }
+    ];
+
+    let orderings = [
+      {
+        id: 2,
+        title: '08/09 (星期一) Mon. 漢堡王',
+        content: '中午供餐',
+        remain: '還有 33 小時 25 分鐘截止'
+      }
+    ];
+
+    let stores = [
+      {
+        id: 3,
+        title: '麥當勞 信義店',
+        phone: '(02) 2641-1332',
+        note: '24 小時內皆可訂購'
+      }
+    ];
+
+    let orderedItems = ordereds.map(item => {
+      let products = item.products.map(product => {
+        return (
+          <span>
+            {product.name}
+            <br />
+            {`數量：${product.amount} 售價：${product.price}`}
+          </span>
+        );
+      });
+
+      return (
+        <ListGroupItem
+          header={item.title}
+          onClick={this._handleOrder.bind(this, item.id)}>
+            {products}
+        </ListGroupItem>
+      );
+    });
+
+    let orderingItems = orderings.map(item => {
+      return (
+        <ListGroupItem
+          header={item.title}
+          onClick={this._handleOrder.bind(this, item.id)}>
+            <span>
+              {item.content}
+              <br />
+              {item.remain}
+            </span>
+        </ListGroupItem>
+      );
+    });
+
+    let recommendStores = stores.map(store => {
+      return (
+        <ListGroupItem
+          header={store.title}
+          onClick={this._handleCreateEvent.bind(this, store.id)}>
+            <span>
+              {store.phone}
+              <br />
+              {store.note}
+            </span>
+        </ListGroupItem>
+      );
+    });
+
     return (
       <Grid>
         <Row>
           <Col md={4}>
             <Panel header={'已訂過的'} bsStyle='success'>
               <ListGroup fill>
-                <ListGroupItem
-                  href='#'
-                  header='08/06 (星期四) Mon. 丁媽 虱目魚店'
-                  onClick={this._handleOrder}>
-                    香煎魚肚飯<br />
-                    數量：1 售價：$100
-                </ListGroupItem>
+                {orderedItems}
               </ListGroup>
             </Panel>
           </Col>
           <Col md={4}>
             <Panel header={'進行中的訂單'} bsStyle='info'>
               <ListGroup fill>
-                <ListGroupItem
-                  href='#'
-                  header='08/09 (星期一) Mon. 漢堡王'
-                  onClick={this._handleOrder}>
-                    中午供餐<br />
-                    還有 33 小時 25 分鐘截止
-                </ListGroupItem>
+                {orderingItems}
               </ListGroup>
             </Panel>
           </Col>
           <Col md={4}>
             <Panel header={'推薦店家'} bsStyle='warning'>
               <ListGroup fill>
-                <ListGroupItem
-                  href='#'
-                  header='麥當勞 信義店'
-                  onClick={this._handleCreateEvent}>
-                    (02) 2641-1332<br />
-                    24 小時內皆可訂購
-                </ListGroupItem>
+                {recommendStores}
               </ListGroup>
             </Panel>
           </Col>
